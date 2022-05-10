a652b0af60698b78c447b6bd9b352c0dfe62d404
ChagaletiSreekanya019 <103957807+ChagaletiSreekanya019@users.noreply.github.com>
2022-05-10 16:27:16 +0530

    Add files via upload

---
+++ divisible 3 and 7.py
@@ -0,0 +1,19 @@
+# i=0
+# while i<=99:
+#     if i%3==0 and i%7==0:
+#         print(i,"navgurukul")
+#     elif i%3==0:
+#         print(i,"nav")
+#     elif i%7==0:
+#         print(i,"gurukul")
+#     i=i+1
+
+    # i=1
+# while i<=10:
+#     j=1
+#     while j<=10:
+#         print(i*j,end=" ")
+#         j=j+1
+#     print()
+#     i=i+1
+
---
+++ divisible by 7.py
@@ -0,0 +1,5 @@
+x=7
+while x<=100:
+    if x%7==0:
+        print(x)
+    x=x+7
