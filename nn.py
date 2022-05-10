2a20b8a52b0edc0a1b99c3cbf5893edc5e695df3
ChagaletiSreekanya019 <103957807+ChagaletiSreekanya019@users.noreply.github.com>
2022-05-10 16:28:53 +0530

    Add files via upload

---
+++ nn.py
@@ -0,0 +1,21 @@
+# i=0
+# while i<=10:
+#     i=i+1
+#     if i%2==0:
+#         continue
+
+# s=5
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5:
+#         if j<i:
+#             print(i,end="")
+#         else:
+#             print(j,end="")
+#         j=j+1
+#     print()
+#     s=s+1
+#     i=i+1
+    
+
